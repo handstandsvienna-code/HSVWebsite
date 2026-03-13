dc051ac957a7219dd830604406b70eb321bdb717
Patrick Moser <p.moser@t-systems.com>
2026-03-13 10:28:13 +0100

    test4

--- script.js
+++ script.js
@@ -308,5 +308,5 @@ document.getElementById("montag").textContent = data.montag;
 document.getElementById("donnerstag").textContent = data.donnerstag;
 document.getElementById("samstag").textContent = data.samstag;
 
-//Test3
+//Test4
 //testMarOfen
